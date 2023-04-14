f0877e8dc64efaa677fe4cb0fc576042efce4820
souravpl500 <111083486+souravpl500@users.noreply.github.com>
2023-04-14 12:37:57 +0530

    Delete src directory

--- src/config/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
-const connectDB = async () => {
-  return mongoose.connect(process.env.DB_URL);
-};
-
-module.exports = connectDB;
