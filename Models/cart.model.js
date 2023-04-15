e50954fb403177cf4afe4d6e4d42ff63a5e24338
souravpl500 <111083486+souravpl500@users.noreply.github.com>
2023-04-15 16:44:00 +0530

    Update cart.model.js

--- Models/cart.model.js
+++ Models/cart.model.js
@@ -2,10 +2,6 @@ const mongoose = require("mongoose");
 
 const cartSchema = mongoose.Schema(
   {
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "user",
-    },
     productId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "product",
