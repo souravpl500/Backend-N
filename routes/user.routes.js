b67ac401247b6c49d366683a24586cc3a390f675
souravpl500 <111083486+souravpl500@users.noreply.github.com>
2023-04-16 11:56:24 +0530

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -11,7 +11,6 @@ userRouter.get("/", async (req, res) => {
     const users = await userModel.find(query);
     res.status(200).send(users);
   } catch (error) {
-    console.log(err);
     res.status(500).send({ err: "Something went wrong" });
   }
 });
@@ -21,7 +20,7 @@ userRouter.post("/register", async (req, res) => {
   try {
     bcrypt.hash(password, 5, async (err, secure_password) => {
       if (err) {
-        console.log(err);
+        res.status(500).send({ err: "Something went wrong" });
       } else {
         const isExist = await userModel.findOne({ email });
         if (isExist) {
@@ -41,7 +40,6 @@ userRouter.post("/register", async (req, res) => {
       }
     });
   } catch (error) {
-    console.log(error);
     return res.status(400).send({
       message: "Something went wrong",
       error: error.message,
@@ -59,7 +57,6 @@ userRouter.post("/login", async (req, res) => {
       bcrypt.compare(password, hashed_password, (err, result) => {
         if (result) {
           const token = jwt.sign({ userID: user[0]._id }, "masai");
-          console.log(user[0].name);
           return res.status(200).send({
             message: "Login Successful",
             username: user[0].name,
@@ -77,7 +74,6 @@ userRouter.post("/login", async (req, res) => {
       });
     }
   } catch (error) {
-    console.log(error);
     return res.status(400).send({
       message: "Something went wrong",
       error: error.message,
